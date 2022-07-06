ea6697e9635fcc94e38a6ec1d6f943210e15946f
Alumno <Alumno@BRII-RIVAS>
2022-07-05 23:18:30 -0300

    Agregar archivos de proyecto.

---
+++ Ejersicio1Debrisarivas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int main()
+{
+	float distancia;
+	int tiempo;
+	float velocidad;
+	cout << "ingrese la velocidad en m/s";
+	cin >> velocidad;
+	cout << "ingrese tiempo por segundos";
+	cin >> tiempo;
+	// Proceso 
+	distancia = velocidad * tiempo;
+	// informar
+	cout << "el auto alcanzo la distancia a velocidad" << velocidad << "en" << tiempo << "seg es de" << distancia << endl;
+	return 0;
+}
+
